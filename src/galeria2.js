c2b0c8cb4678b39362e820ec8cc6933d71ff0c93
Pablo Duchovny <pablo.duchovny@gmail.com>
2020-02-01 11:36:09 -0300

    primer commit

--- src/galeria2.js
+++ src/galeria2.js
@@ -5,39 +5,38 @@ let botContact = document.getElementById("contactBot");
 let botHamb = document.getElementById("hamb");
 let modal = document.getElementById("myModal");
 
-botHamb.addEventListener("click", function(event) {
-  event.preventDefault();
-  modal.style.display = "block";
-  document.getElementById("nav").classList.add("show");
-});
 botArtist.addEventListener("click", function(event) {
   event.preventDefault();
   modal.style.display = "none";
-  document.getElementById("nav").classList.remove("show");
+  document.getElementById("myNavbar").classList.remove("show");
   document.getElementById("artist").scrollIntoView({ behavior: "smooth" });
 });
 botHome.addEventListener("click", function(event) {
   event.preventDefault();
   modal.style.display = "none";
-  document.getElementById("nav").classList.remove("show");
+  document.getElementById("myNavbar").classList.remove("show");
   document.getElementById("home").scrollIntoView({ behavior: "smooth" });
 });
 botWork.addEventListener("click", function(event) {
   event.preventDefault();
   modal.style.display = "none";
-  document.getElementById("nav").classList.remove("show");
+  document.getElementById("myNavbar").classList.remove("show");
   document.getElementById("work").scrollIntoView({ behavior: "smooth" });
 });
 botContact.addEventListener("click", function(event) {
   event.preventDefault();
   modal.style.display = "none";
-  document.getElementById("nav").classList.remove("show");
+  document.getElementById("myNavbar").classList.remove("show");
   document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
 });
-
+botHamb.addEventListener("click", function(event) {
+  event.preventDefault();
+  modal.style.display = "block";
+  document.getElementById("nav").classList.add("show");
+});
 window.onclick = function(event) {
   if (event.target == modal) {
-    document.getElementById("nav").classList.remove("show");
+    document.getElementById("myNavbar").classList.remove("show");
     modal.style.display = "none";
   }
 };
